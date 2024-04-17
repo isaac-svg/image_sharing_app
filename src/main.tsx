d39b14ea50d5a51aa6384aaf15ee4548cc501a52
isaac-svg <isaacsakyi009@gmail.com>
2024-04-17 00:01:49 +0000

    Fix Routing Issue

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./screens/Login.tsx";
 import Register from "./screens/Register.tsx";
 import Content from "./components/Content.tsx";
@@ -14,7 +14,7 @@ import { action as loginAction } from "./screens/Login.tsx";
 import { action as uploadAction } from "./screens/Upload.tsx";
 import Upload from "./screens/Upload.tsx";
 import Update from "./screens/Update.tsx";
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
